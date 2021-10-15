66af0f9861c71d18c6c8ad2e1ed89d4e1c6b529c
David <zdavid1@myges.fr>
2021-10-15 22:42:54 +0200

    initial commit main struct joueur

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct Joueur
+{
+    int experience;
+    int niveau;
+    int pointDeVieCourant;
+    int pointDeVieMax;
+    int inventaire[10];
+};
+typedef struct Joueur joueurStructure;
+
+int main() {
+    printf("Hello");
+    return 0;
+}
+
