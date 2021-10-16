4eb949879850641dbcbb93454fd79127a488f92b
David <zdavid1@myges.fr>
2021-10-16 18:45:21 +0200

    Ressource Collect in english please

--- src/main.c
+++ src/main.c
@@ -3,48 +3,56 @@
 #include <stdlib.h>
 #include <time.h>
 
-struct Joueur {
+struct Player {
     int experience;
-    int niveau;
-    int pointDeVieCourant;
-    int pointDeVieMax;
-    int inventaire[10][3];
+    int level;
+    int currentHealthPoints;
+    int maxHealthpoints;
+    int inventory[10][3];
 };
-typedef struct Joueur Joueur;
+typedef struct Player Player;
 
-void StatDepartJoueur(Joueur *);
+void StatDepartPlayer(Player *);
 
-void CollecteRessources(Joueur *);
+void CollecteRessources(Player *);
+
+void PrintInventory(Player);
 
 int main(int argc, const char *argv[]) {
-    Joueur joueurStruct;
-    //Stat du joueur au début de la partie
-    StatDepartJoueur(&joueurStruct);
-
+    Player PlayerStruct;
+    //Stat du Player au début de la partie
+    StatDepartPlayer(&PlayerStruct);
     //CollecteRessources
-    CollecteRessources(&joueurStruct);
+    CollecteRessources(&PlayerStruct);
+    //printInventaire
+    PrintInventory(PlayerStruct);
     return 0;
 }
-
-void StatDepartJoueur(Joueur *joueurDebut) {
-    joueurDebut->niveau = 1;
-    joueurDebut->pointDeVieCourant = 100;
-    joueurDebut->pointDeVieMax = 100;
-    joueurDebut->inventaire[0][0] = 1;
-    joueurDebut->inventaire[0][1] = 10;
-    joueurDebut->inventaire[0][2] = 1;
-    joueurDebut->inventaire[1][0] = 2;
-    joueurDebut->inventaire[1][1] = 10;
-    joueurDebut->inventaire[1][2] = 0;
-    joueurDebut->inventaire[2][0] = 3;
-    joueurDebut->inventaire[2][1] = 10;
-    joueurDebut->inventaire[2][2] = 0;
-    joueurDebut->inventaire[3][0] = 4;
-    joueurDebut->inventaire[3][1] = 10;
-    joueurDebut->inventaire[3][2] = 0;
+void PrintInventory(Player p){
+    for (int i = 0; i < 5; i++) {
+        printf("Votre inventaire : %d",p.inventory[i][0]);
+    }
 }
 
-void CollecteRessources(Joueur *inventoryCollect) {
+void StatDepartPlayer(Player *PlayerDebut) {
+    PlayerDebut->level = 1;
+    PlayerDebut->currentHealthPoints = 100;
+    PlayerDebut->maxHealthpoints = 100;
+    PlayerDebut->inventory[0][0] = 1;
+    PlayerDebut->inventory[0][1] = 10;
+    PlayerDebut->inventory[0][2] = 1;
+    PlayerDebut->inventory[1][0] = 2;
+    PlayerDebut->inventory[1][1] = 10;
+    PlayerDebut->inventory[1][2] = 0;
+    PlayerDebut->inventory[2][0] = 3;
+    PlayerDebut->inventory[2][1] = 10;
+    PlayerDebut->inventory[2][2] = 0;
+    PlayerDebut->inventory[3][0] = 4;
+    PlayerDebut->inventory[3][1] = 10;
+    PlayerDebut->inventory[3][2] = 0;
+}
+
+void CollecteRessources(Player *inventoryCollect) {
     int nextBox = 6;
 
     //On verifie l'item
@@ -52,33 +60,29 @@ void CollecteRessources(Joueur *inventoryCollect) {
 
     if(indexItem != -1){
         //On verifie la durabilite
-     int durability = VerifDurability(inventoryCollect, nextBox, indexItem);
-     if(durability != -1){
-         //On ajoute la ressource
-     int addResource = AddInventoryResources(inventoryCollect,nextBox);
-     if(addResource != -1){
-         printf("%d Ressources ajoute ! ",addResource);
-     }else{
-         printf("Inventaire pleins ! ");
-     }
-     }else{
-         printf("Vous n'avez plus de durabilite sur l'outil necessaire !");
-     }
+        int durability = VerifDurability(inventoryCollect, nextBox, indexItem);
+        if(durability != -1){
+            //On ajoute la ressource
+            int addResource = AddInventoryResources(inventoryCollect,nextBox);
+            if(addResource != -1){
+                printf("%d Ressources ajoute ! ",addResource);
+            }else{
+                printf("inventory pleins ! ");
+            }
+        }else{
+            printf("Vous n'avez plus de durabilite sur l'outil necessaire !");
+        }
     }else{
         printf("Vous n'avez pas l'outil necessaire !");
     }
-    for (int i = 0; i < sizeof(inventoryCollect); i++) {
-        printf("VOTRE INVENTAIRE : %d ",inventoryCollect->inventaire[i][0]);
-    }
-
 }
 
-int VerifResource(Joueur *inventoryCollect,int nextBox) {
+int VerifResource(Player *inventoryCollect,int nextBox) {
     int resource = ResourceNecessary(nextBox);
-    //on verifie si le joueur a deja l'element
+    //on verifie si le Player a deja l'element
     for (int i = 0; i < sizeof(inventoryCollect); i++) {
-        //on verifie si le joueur a deja de la pierre
-        if (inventoryCollect->inventaire[i][0] == resource) {
+        //on verifie si le Player a deja de la pierre
+        if (inventoryCollect->inventory[i][0] == resource) {
             return i;
         }
     }
@@ -179,67 +183,62 @@ int ResourceNecessary(int nextBox){
     return -1;
 }
 
-int VerifItem(Joueur *inventoryCollect, int nextBox) {
+int VerifItem(Player *inventoryCollect, int nextBox) {
     //On verifie si il l'item
     int *tools = ToolsNecessary(nextBox);
     for (int i = 0; i < sizeof(inventoryCollect); i++) {
         for (int j = 0; j < sizeof(tools); j++) {
-            if (inventoryCollect->inventaire[i][0] == tools[j]) {
+            if (inventoryCollect->inventory[i][0] == tools[j]) {
                 return i;
             }
         }
     }
 }
 
-int VerifDurability(Joueur *inventoryCollect, int nextBox, int item) {
+int VerifDurability(Player *inventoryCollect, int nextBox, int item) {
     int percentage =0;
     if (nextBox > 2 && nextBox < 6) {
-         percentage = 10;
+        percentage = 10;
     }
     if (nextBox > 5 && nextBox < 9) {
-         percentage = 20;
+        percentage = 20;
     }
     if (nextBox > 8 && nextBox < 12) {
-         percentage = 40;
+        percentage = 40;
     }
     //si la pelle à de la durabilité
-    if (inventoryCollect->inventaire[item][1] != 0) {
+    if (inventoryCollect->inventory[item][1] != 0) {
         //on enleve % de durabilité
-        inventoryCollect->inventaire[item][1] -= inventoryCollect->inventaire[item][1] / percentage;
+        inventoryCollect->inventory[item][1] -= inventoryCollect->inventory[item][1] / percentage;
         return 0;
     }
     return -1;
 }
 
-int AddInventoryResources(Joueur *inventoryCollect, int nextBox) {
+int AddInventoryResources(Player *inventoryCollect, int nextBox) {
 
     srand(time(NULL));
-     int indexResources = VerifResource(inventoryCollect,nextBox);
-     int randomResourceNumber = rand() % ((5) - 1) + 1;
+    int indexResources = VerifResource(inventoryCollect,nextBox);
+    int randomResourceNumber = rand() % ((5) - 1) + 1;
     //On verifie si il y a de la pierre et on stack de 1 à 4
     if (indexResources != -1) {
-        inventoryCollect->inventaire[indexResources][0] += randomResourceNumber;
-        inventoryCollect->inventaire[indexResources][1] = 0;
-        inventoryCollect->inventaire[indexResources][2] = 0;
+        inventoryCollect->inventory[indexResources][0] += randomResourceNumber;
+        inventoryCollect->inventory[indexResources][1] = 0;
+        inventoryCollect->inventory[indexResources][2] = 0;
         //on verifie si il y a plus de 20 on met a 20
-        if (inventoryCollect->inventaire[indexResources][0] > 20) {
-            inventoryCollect->inventaire[indexResources][0] = 20;
+        if (inventoryCollect->inventory[indexResources][0] > 20) {
+            inventoryCollect->inventory[indexResources][0] = 20;
         }
     } else {
-        //On verifie la disponibilité dans l'inventaire du joueur et on ajouter 1 à 4
+        //On verifie la disponibilité dans l'inventory du Player et on ajouter 1 à 4
         if (sizeof(inventoryCollect) != 10) {
-            inventoryCollect->inventaire[sizeof(inventoryCollect)][0] +=
+            inventoryCollect->inventory[sizeof(inventoryCollect)][0] +=
                     randomResourceNumber;
-            inventoryCollect->inventaire[sizeof(inventoryCollect)][1] = 0;
-            inventoryCollect->inventaire[sizeof(inventoryCollect)][2] = 0;
+            inventoryCollect->inventory[sizeof(inventoryCollect)][1] = 0;
+            inventoryCollect->inventory[sizeof(inventoryCollect)][2] = 0;
         }else{
             return -1;
         }
     }
     return randomResourceNumber;
 }
-
-
-
-
-
