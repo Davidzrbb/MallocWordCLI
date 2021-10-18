f512b2066fff653aa2db1a7a347e40927879c347
David <zdavid1@myges.fr>
2021-10-18 21:02:22 +0200

    inventaire with struct

---
+++ include/include.h
@@ -0,0 +1,96 @@
+#define MALLOC_WORLD_CLI_COLLECT_RESSOURCE_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+typedef struct Player Player;
+
+struct Weapon{
+    char * name;
+    int id;
+    int damage;
+    float max_durability;
+    float actual_durabiulity;
+};
+typedef struct Weapon Weapon;
+
+struct Tools{
+    char * name;
+    int id;
+    float max_durability;
+    float actual_durabiulity;
+};
+typedef struct Tools Tools;
+
+struct Resource{
+    int id;
+    int quantity;
+};
+typedef struct Resource Resource;
+
+struct AllObjectInventory{
+    struct  Weapon weapon;
+    struct  Tools tools;
+    struct  Resource resource;
+};
+typedef struct AllObjectInventory AllObjectInventory;
+
+struct Player {
+    int experience;
+    int level;
+    int currentHealthPoints;
+    int maxHealthpoints;
+    AllObjectInventory inventory[10];
+};
+
+enum entity {
+    PORTAL1_2 = -3,
+    PORTAL2_3 = -2,
+    IMPASSABLE = -1,
+    FREE = 0,
+    PLAYER = 1,
+    NPC = 2,
+    PLANT1 = 3,
+    ROCK1= 4,
+    WOOD1=5,
+    PLANT2=6,
+    ROCK2=7,
+    WOOD2=8,
+    PLANT3=9,
+    ROCK3=10,
+    WOOD3=11,
+    END8BOSS=99,
+};
+typedef enum entity entity;
+
+enum resource {
+    FIR = 5,
+    ROCK = 6,
+    GRASS = 7,
+    BEECH = 16,
+    IRON = 17,
+    LAVANDER= 18,
+    OAK = 27,
+    DIAMOND= 28,
+    HEMP=29,
+};
+typedef enum resource resource;
+enum tools {
+    WOODEN_PICKAXE = 2,
+    WOODEN_BILLHOOK = 3,
+    WOODEN_AX = 4,
+    STONE_PICKAXE = 12,
+    STONE_BILLHOOK = 13,
+    STONE_AX= 14,
+    IRON_PICKAXE = 23,
+    IRON_BILLHOOK= 24,
+    IRON_AX =25,
+};
+typedef enum tools tools;
+
+void InitPlayer(Player * );
+void printTest(Player);
--- src/main.c
+++ src/main.c
@@ -1,50 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "../include/include.h";
 
-struct Player
-{
-    int experience;
-    int level;
-    int currentHealthPoints;
-    int maxHealthpoints;
-    int inventory[10][3];
-};
-typedef struct Player Player;
-
-void initPlayer(Player*);
-
-
+#include <string.h>
 
 int main(int argc, const char *argv[]) {
-    Player playerStruct;
-    //Stat du joueur au début de la partie
-    initPlayer(&playerStruct);
-    
+    Player PlayerStruct;
+    AllObjectInventory AllObjectInventoryStruct;
+    //Stat du Player au début de la partie
+    InitPlayer(&PlayerStruct);
+//Print
+    printTest(PlayerStruct);
     return 0;
 }
+
 void InitPlayer(Player* firstPlayer) {
+
+    AllObjectInventory *WeaponSword = malloc(sizeof(AllObjectInventory));
+    WeaponSword->weapon.name = "sword";
+    WeaponSword->weapon.damage= 1;
+    WeaponSword->weapon.max_durability = 10;
+    WeaponSword->weapon.actual_durabiulity  = 10;
+    WeaponSword->weapon.id = 1;
+
+    AllObjectInventory *ToolsPickaxe = malloc(sizeof(AllObjectInventory));
+    ToolsPickaxe->tools.name ="pickaxe";
+    ToolsPickaxe->tools.id = WOODEN_PICKAXE;
+    ToolsPickaxe->tools.max_durability=10;
+    ToolsPickaxe->tools.actual_durabiulity=10;
+
+    AllObjectInventory  *ToolsBillhook = malloc(sizeof(AllObjectInventory));
+    ToolsBillhook->tools.name ="billhook";
+    ToolsBillhook->tools.id = WOODEN_BILLHOOK;
+    ToolsBillhook->tools.max_durability=10;
+    ToolsBillhook->tools.actual_durabiulity=0;
+
+    AllObjectInventory *ToolsAx = malloc(sizeof(AllObjectInventory));
+    ToolsAx->tools.name ="ax";
+    ToolsAx->tools.id = WOODEN_AX;
+    ToolsAx->tools.max_durability=10;
+    ToolsAx->tools.actual_durabiulity=10;
+
     firstPlayer->level = 1;
     firstPlayer->currentHealthPoints = 100;
     firstPlayer->maxHealthpoints = 100;
-    firstPlayer->inventory[0][0] =  1;
-    firstPlayer->inventory[0][1] =  10;
-    firstPlayer->inventory[0][2] =  1;
-
-    firstPlayer->inventory[1][0] =  2;
-    firstPlayer->inventory[1][1] =  10;
-    firstPlayer->inventory[1][2] =  0;
-
-    firstPlayer->inventory[2][0] =  3;
-    firstPlayer->inventory[2][1] =  10;
-    firstPlayer->inventory[2][2] =  0;
-
-    firstPlayer->inventory[3][0] =  4;
-    firstPlayer->inventory[3][1] =  10;
-    firstPlayer->inventory[3][2] =  0;
+    firstPlayer->inventory[0] = *WeaponSword ;
+    firstPlayer->inventory[1] = *ToolsPickaxe ;
+    firstPlayer->inventory[2] = *ToolsBillhook ;
+    firstPlayer->inventory[3] = *ToolsAx ;
 }
 
-
-
-
-
-
+void printTest(Player PlayerStruct){
+    printf("Votre arme : %s \n c'est dommage : %d",PlayerStruct.inventory[0].weapon.name,PlayerStruct.inventory[0].weapon.damage);
+}
