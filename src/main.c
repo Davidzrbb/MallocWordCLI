14ae4e8dd4d888a7c0173d087bce9d72ad934393
David <zdavid1@myges.fr>
2021-11-15 20:20:35 +0100

    nettoyage avant merge master

--- src/main.c
+++ src/main.c
@@ -1,98 +1,63 @@
 #include "../include/include.h"
 #include "../include/pnjService.h"
 #include "../include/collecteResource.h"
+
 int main(int argc, const char *argv[]) {
     Player PlayerStruct;
     Item ItemStruct;
     //PnjLinkedList stock;
-    PnjLinkedList *stock = malloc(sizeof (PnjLinkedList));
+    PnjLinkedList *stock = malloc(sizeof(PnjLinkedList));
     initStructStock(stock);
     //Stat du Player au début de la partie
     InitPlayer(&PlayerStruct);
-    //move for collect
-    MovePlayer(&PlayerStruct);
     //PnjFix
-    pnjChoice(&PlayerStruct,stock);
-    //Print
-    //printTest(PlayerStruct);
+    //pnjChoice(&PlayerStruct, stock);
     return 0;
 }
-void InitPlayer(Player* firstPlayer) {
+
+void InitPlayer(Player *firstPlayer) {
     Item *WeaponSword = malloc(sizeof(Item));
-    WeaponSword->weapon.damage= 1;
-    WeaponSword->weapon.name ="epee en bois";
+    WeaponSword->weapon.damage = 1;
+    WeaponSword->weapon.name = "epee en bois";
     WeaponSword->weapon.max_durability = 10;
-    WeaponSword->weapon.actual_durabiulity  = 10;
+    WeaponSword->weapon.actual_durabiulity = 10;
     WeaponSword->weapon.id = WOODEN_SWORD;
     WeaponSword->type = WEAPON;
 
     Item *ToolsPickaxe = malloc(sizeof(Item));
     ToolsPickaxe->tools.name = "pioche en bois";
-    ToolsPickaxe->tools.max_durability=10;
-    ToolsPickaxe->tools.actual_durabiulity=5;
-    ToolsPickaxe->tools.id= STONE_PICKAXE;
-    ToolsPickaxe->type=TOOL;
+    ToolsPickaxe->tools.max_durability = 10;
+    ToolsPickaxe->tools.actual_durabiulity = 5;
+    ToolsPickaxe->tools.id = STONE_PICKAXE;
+    ToolsPickaxe->type = TOOL;
 
 
-    Item  *ToolsBillhook = malloc(sizeof(Item));
-    ToolsBillhook->tools.name ="serpette en bois";
-    ToolsBillhook->tools.max_durability=10;
-    ToolsBillhook->tools.actual_durabiulity=10;
-    ToolsBillhook->tools.id= WOODEN_BILLHOOK;
-    ToolsBillhook->type= TOOL;
+    Item *ToolsBillhook = malloc(sizeof(Item));
+    ToolsBillhook->tools.name = "serpette en bois";
+    ToolsBillhook->tools.max_durability = 10;
+    ToolsBillhook->tools.actual_durabiulity = 10;
+    ToolsBillhook->tools.id = WOODEN_BILLHOOK;
+    ToolsBillhook->type = TOOL;
 
     Item *ToolsAx = malloc(sizeof(Item));
-    ToolsAx->tools.name = malloc(sizeof (char *));
-    ToolsAx->tools.name ="hache en bois";
-    ToolsAx->tools.max_durability=10;
-    ToolsAx->tools.actual_durabiulity=10;
+    ToolsAx->tools.name = malloc(sizeof(char *));
+    ToolsAx->tools.name = "hache en bois";
+    ToolsAx->tools.max_durability = 10;
+    ToolsAx->tools.actual_durabiulity = 10;
     ToolsAx->tools.id = WOODEN_AX;
-    ToolsAx->type= TOOL;
-
-
-    Item *Sapin = malloc(sizeof(Item));
-    Sapin->resource.name = malloc(sizeof (char *));
-    Sapin->resource.name ="sapin";
-    Sapin->resource.id = FIR;
-    Sapin->resource.quantity=3;
-    Sapin->type= RESOURCE;
-
-    Item *Pierre = malloc(sizeof(Item));
-    Pierre->resource.name = malloc(sizeof (char *));
-    Pierre->resource.name ="pierre";
-    Pierre->resource.id = ROCK;
-    Pierre->resource.quantity=10;
-    Pierre->type= RESOURCE;
-
+    ToolsAx->type = TOOL;
 
     firstPlayer->level = 1;
     firstPlayer->currentHealthPoints = 100;
     firstPlayer->maxHealthpoints = 100;
-    firstPlayer->inventory[0] = *WeaponSword ;
-    firstPlayer->inventory[1] = *ToolsPickaxe ;
-    firstPlayer->inventory[2] = *ToolsBillhook ;
-    firstPlayer->inventory[3] = *ToolsAx ;
-    firstPlayer->inventory[4] = *Sapin ;
-    firstPlayer->inventory[5] = *Pierre ;
-}
-
-
-void MovePlayer(Player *PlayerStruct) {
-    int nextBox = 3;
-   // printf("Votre arme %d",PlayerStruct.inventory[0].weapon.id);
-   //collecteRessources
-    collecteRessources(PlayerStruct, nextBox);
-}
-
-
-
-
-void printTest(Player PlayerStruct){
-    for (int i = 0; i<sizeof(PlayerStruct.inventory[i].type);i++){
-            //printf("Vos item %s", PlayerStruct.inventory[i].tools.name);
-        }
+    firstPlayer->inventory[0] = *WeaponSword;
+    firstPlayer->inventory[1] = *ToolsPickaxe;
+    firstPlayer->inventory[2] = *ToolsBillhook;
+    firstPlayer->inventory[3] = *ToolsAx;
 
 }
+
+
 
 
 
