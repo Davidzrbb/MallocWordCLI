3d64c416dedbdc3f8274203fb691c2271cf3d716
Lucas BLASON <lucas.blason@eos-france.com>
2021-11-19 15:09:48 +0100

    create map save

---
+++ include/save.h
@@ -0,0 +1,15 @@
+//
+// Created by LBZ on 19/11/2021.
+//
+
+#ifndef MALLOCWORLD_SAVE_H
+#define MALLOCWORLD_SAVE_H
+
+#include "include.h"
+
+void saveFile(int *mapSize,int *** map_list);
+
+
+#endif //MALLOCWORLD_SAVE_H
+
+
--- src/save.c
+++ src/save.c
@@ -2,3 +2,25 @@
 // Created by LBZ on 19/11/2021.
 //
 
+#include "../include/save.h"
+
+
+savefile(int * mapSize,int *** map_list){
+
+    File * file = fopen("saveFile.txt","a");
+
+    fprintf( file, "===MAP===");
+
+    for (int i = 0; i < 3; ++i) {
+        fprintf( file, "--ZONE%d--",i);
+        for (int j = 0; j < mapSize[i]; ++j) {
+            fprintf(file,"\n");
+            for (int k = 0; k <  mapSize[i]; ++k) {
+                fprintf(file, "%d ",map_list[i][j][k])
+            }
+        }
+    }
+
+
+    fprint
+}
