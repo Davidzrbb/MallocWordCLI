9808cc01278981076635e980346eaee925cc4c42
david <zdavid1@myges.fr>
2021-11-28 15:51:03 +0100

    debug spawn monster

--- src/map.c
+++ src/map.c
@@ -53,40 +53,45 @@ void generate_land(int **map_list, int x, int y, int map_id, int **map_cpy) {
                     map_list[i][j] = ROCK3;
                     map_cpy[i][j] = ROCK3;
                 }
-            } else if (perlin < 0.9) {
-                randMonster = rand() % 3;
-                if (i == 0) {
-                    map_list[i][j] = randMonster + 90;
-                    map_cpy[i][j] = randMonster + 90;
-                } else if (i == 1) {
-                    map_list[i][j] = randMonster + 93;
-                    map_cpy[i][j] = randMonster + 93;
-                } else {
-                    map_list[i][j] = randMonster + 96;
-                    map_cpy[i][j] = randMonster + 96;
-                }
             } else {
                 map_list[i][j] = IMPASSABLE;
                 map_cpy[i][j] = IMPASSABLE;
             }
         }
     }
-
     map_list[0][0] = NPC;
     map_cpy[0][0] = NPC;
     if (map_id == 1) {
+        map_list[x - 2][0] = 90;
+        map_cpy[x - 2][0] = 90;
+        map_list[x - 2][1] = 91;
+        map_cpy[x - 2][1] = 91;
+        map_list[x - 1][1] = 92;
+        map_cpy[x - 1][1] = 92;
         map_list[x - 2][y - 2] = PORTAL1_2;
         map_cpy[x - 2][y - 2] = PORTAL1_2;
     } else if (map_id == 2) {
+        map_list[x - 2][0] = 93;
+        map_cpy[x - 2][0] = 93;
+        map_list[x - 2][1] = 94;
+        map_cpy[x - 2][1] = 94;
+        map_list[x - 1][1] = 95;
+        map_cpy[x - 1][1] = 95;
         map_list[1][y - 2] = PORTAL1_2;
         map_list[x - 2][y - 2] = PORTAL2_3;
         map_cpy[1][y - 2] = PORTAL1_2;
         map_cpy[x - 2][y - 2] = PORTAL2_3;
     } else {
+        map_list[x - 2][0] = 96;
+        map_cpy[x - 2][0] = 96;
+        map_list[x - 2][1] = 97;
+        map_cpy[x - 2][1] = 97;
+        map_list[x - 1][1] = 98;
+        map_cpy[x - 1][1] = 98;
         map_list[1][y - 2] = PORTAL2_3;
-        map_list[x - 2][1] = END_BOSS;
+        map_list[x - 1][0] = END_BOSS;
         map_cpy[1][y - 2] = PORTAL2_3;
-        map_cpy[x - 2][1] = END_BOSS;
+        map_cpy[x - 1][0] = END_BOSS;
     }
     if (map_id == 1) {
         map_list[1][0] = 1;
